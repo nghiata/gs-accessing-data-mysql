9801dae22b4361bb7ca010fbd606e21b3146f590
nghiata <nghia.ta@propzy.com>
2020-03-16 17:21:39 +0700

    [user] update, delete

--- complete/src/main/java/com/example/accessingdatamysql/MainController.java
+++ complete/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -3,10 +3,14 @@ package com.example.accessingdatamysql;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,5 +44,22 @@ public class MainController {
 	@GetMapping(path="/{id}")
 	public @ResponseBody Optional<User> getUser(@PathVariable int id) {
 		return userRepository.findById(id);
+	}
+	
+	@PutMapping(path="/{id}")
+	public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable int id) {
+		Optional<User> userOptional = userRepository.findById(id);
+		
+		if (!userOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		
+		user.setId(id);
+		userRepository.save(user);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
 	}
 }
